f793a5326f5db1772c8624008bfde379390c22a1
Alex <alex@Alexs-MacBook-Air.local>
2025-01-26 15:13:52 -0600

    added simple header and footer

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './assets/Header'
+import Footer from './assets/Footer'
+import Food from './assets/Food'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,8 @@ function App() {
   return (
     <>
       <Header></Header>
-        
+      <Food></Food>
+      <Footer></Footer>
     </>
   )
 }
---
+++ react-practice/src/assets/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} React Practice</p>
+        </footer>
+    );
+}
+
+export default Footer
