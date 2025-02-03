a7e1844c7ddc82d7d86af6b396ddaf419702dd3a
Alex <alex@Alexs-MacBook-Air.local>
2025-02-02 23:52:15 -0600

    Added component with 4 onchange events

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MyComponent from './assets/MyComponent'
 
-import Header from './assets/Header'
 
 function App() {
 
   return (
     <>
-        
+        <MyComponent />
     </>
   )
 }
