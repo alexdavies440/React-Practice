e114d9334833604a846de372ea9cec4530193977
Alex <alex@Alexs-MacBook-Air.local>
2025-02-08 15:07:32 -0600

    Added search button and handling for blank search

--- react-practice/src/assets/Component.jsx
+++ react-practice/src/assets/Component.jsx
@@ -9,13 +9,15 @@ const BASE_URL = 'https://www.omdbapi.com/?apikey=b0901f52&';
 export default function Component() {
 
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState("");
 
     // const url = 'https://www.omdbapi.com/?apikey=b0901f52&s=shrek';
 
     // const url = 'https://fakestoreapi.com/products/';
 
+
     function getData() {
-        fetch(BASE_URL + "s=shrek2")
+        fetch(BASE_URL + "s=" + search)
             .then((response) => {
                 return response.json();
             })
@@ -26,14 +28,27 @@ export default function Component() {
             .catch((error) => console.log(error));
     }
 
+    function handleChange(event) {
+        setSearch(event.target.value);
+    }
 
-    useEffect(() => {
-        getData();
-      }, []);
+    function handleSearch() {
+        if (search !== "") {
+            getData();
+        } else setMovies([]);
+    }
+
+
+    // useEffect(() => {
+    //     getData();
+    //   }, []);
     
 
     return(
         <div>
+            <input type="text" onChange={handleChange} value={search}/>
+            <button className="search-button" onClick={handleSearch}>Search</button>
+
             <ul>
                 {movies.map((movie, id) => {
                     return <li key={id} className="list-item">
