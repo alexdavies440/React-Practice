256c9285d006389349480c966a4a06430b132aaa
Alex <alex@Alexs-MacBook-Air.local>
2025-02-08 23:45:48 -0600

    Successfully send String from Spring Boot to React frontend

--- react-practice/src/assets/Component.jsx
+++ react-practice/src/assets/Component.jsx
@@ -2,54 +2,24 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 
-const BASE_URL = 'https://www.omdbapi.com/?apikey=';
-
-// Your API key
-const API_KEY = '';
+const url = 'http://localhost:8080/jobs'
 
 
 
 export default function Component() {
 
-    const [movies, setMovies] = useState([]);
-    const [search, setSearch] = useState("");
+    const [serverData, setServerData] = useState(null);
 
-    function getData() {
-        fetch(BASE_URL + API_KEY + "&s=" + search)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                console.log(data);
-              setMovies(data.Search);
-            })
-            .catch((error) => console.log(error));
-    }
-
-    function handleChange(event) {
-        setSearch(event.target.value);
-    }
-
-    function handleSearch() {
-        if (search !== '' && API_KEY !== '') {
-            getData();
-        } else setMovies([]);
-    }
-
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.text())
+        .then(data => setServerData(data));
+    }, []);
     return(
         <div>
-            <input type="text" onChange={handleChange} value={search}/>
-            <button className="search-button" onClick={handleSearch}>Search</button>
-            <h2>{API_KEY === '' ?  "Please provide an API Key" : null}</h2>
-            <ul>
-                {movies.map((movie, id) => {
-                    return <li key={id} className="list-item">
-                        {movie.Title} ({movie.Year})
-                        <br />
-                        <img className="movie-poster" src={movie.Poster} alt="movie poster" />
-                    </li>
-                })}
-            </ul>
+            {serverData && (
+                <h1>Test: {serverData}</h1>
+            )}
         </div>
     );
 }
