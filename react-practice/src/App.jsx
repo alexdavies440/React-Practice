45c26e6b79bcd12220cb13571cd9b62c26c88734
Alex <alex@Alexs-MacBook-Air.local>
2025-02-08 19:43:23 -0600

    working todo app, working on styling

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import TodoList from './assets/TodoList'
 
 
 function App() {
 
   return (
     <>
-        
+      <TodoList />
     </>
   )
 }
