87efaa7726beb45367ed71ddffed8a5a4c5755b0
Alex <alex@Alexs-MacBook-Air.local>
2025-02-09 12:59:40 -0600

    Reading event data from Spring Boot server and rendering on page as events list

--- react-practice/src/assets/Component.jsx
+++ react-practice/src/assets/Component.jsx
@@ -2,24 +2,35 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 
-const url = 'http://localhost:8080/jobs'
+const url = 'http://localhost:8080/test'
 
 
 
 export default function Component() {
 
-    const [serverData, setServerData] = useState(null);
+    const [serverData, setServerData] = useState([]);
 
     useEffect(() => {
         fetch(url)
-        .then(res => res.text())
+        .then(res => res.json())
         .then(data => setServerData(data));
     }, []);
+
     return(
         <div>
-            {serverData && (
-                <h1>Test: {serverData}</h1>
-            )}
+            <h1>Events</h1>
+            <hr />
+            <div>{serverData.map((item) => {
+                return (
+                    <div>
+                    <h3>{item.name}</h3>
+                        <ul>
+                            <li>Description: {item.description}</li>
+                            <li>Type: {item.type.toLowerCase()}</li>
+                        </ul>
+                        </div>
+                )
+            })}</div>
         </div>
     );
 }
